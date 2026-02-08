febf37f4b08f5241c94f0cf6f3bd5269eb649853
Luke Siverling <siverlingluke@gmail.com>
2026-02-07 22:22:18 -0600

    making some EOS changes and added SRK

---
+++ src/main/java/eos/CubicEOS.java
@@ -0,0 +1,11 @@
+package eos;
+
+public class CubicEOS {
+    /* Cubic Equation of State
+    Give it an a(T), b, eta, and sigma.
+    */
+
+   public double calcPressure() {
+    double pressure = 
+   }
+}
--- src/main/java/eos/SoaveRK.java
+++ src/main/java/eos/SoaveRK.java
@@ -1,14 +1,16 @@
 package eos;
 
-public class RedlichKwong {
+public class SoaveRK {
     /*
-    RedlichKwong EOS
+    SoaveRedlichKwong EOS
 
     P = RT/(V - b) - a(T) / V*(V+b)
     a(T) = psi * alpha(Tr)* R^2 * Tc^2 / Pc
     b = omega * R * Tc / Pc
-    For RK, alpha(Tr) = Tr^(-1/2)
-    for RK, sigma = 1, eta = 0, omega = 0.08664, psi = 0.42748, Zc = 1/3
+    For SRK, alpha(Tr) = alphaSRK(Tr; w)
+    alphaSRK(Tr; w) = (1 + (0.480 +1.574*w - 0.176*w^2)(1 - Tr^0.5))^2
+    w from tables or solved from w = -1.0 - log10(Pr*) at Tr = 0.7
+    for SRK, sigma = 1, eta = 0, omega = 0.08664, psi = 0.42748, Zc = 1/3
     */
 
     double sigma = 1;
@@ -20,17 +22,18 @@ public class RedlichKwong {
     double V; //volume, ft^3
     double P; //pressure, atm
 
-    public RedlichKwong(double T, double V  ) {
+    public SoaveRK(double T, double V  ) {
         this.T = T+459.67;
         this.V = V;
     }
 
-    public double calcPressure(double Tc, double Pc) {
+    public double calcPressure(double Tc, double Pc, double w) {
         /* Must supply critical temperature (Tc) and critical pressure (Pc) */
 
         double Tr = T / Tc;
 
-        double a_T = psi*Math.pow(Tr,-0.5)*Math.pow(0.7302,2)*Math.pow(Tc,2)/Pc;
+        double alpha_SRK = Math.pow((1+ (0.480 + 1.574 * w - 0.176 * Math.pow(w,2))*(1 - Math.pow(Tr,0.5))),2);
+        double a_T = psi*alpha_SRK*Math.pow(0.7302,2)*Math.pow(Tc,2)/Pc;
         double b = omega*0.7302*Tc/Pc;
         this.P = 0.7302*T/(V - b) - a_T / (V*(V+b));
 
@@ -38,8 +41,8 @@ public class RedlichKwong {
     }
 
     public static void main(String[] args) {
-        RedlichKwong test = new RedlichKwong(122, 2);
-        test.calcPressure(343.1, 45.4);
+        SoaveRK test = new SoaveRK(122, 2);
+        test.calcPressure(343.1, 45.4, 0.012);
         System.out.println(test.P);
     }
 }
