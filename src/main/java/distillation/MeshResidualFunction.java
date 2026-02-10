bd4625670832dfbe38669abc664535daa0e7d0be
Luke Siverling <siverlingluke@gmail.com>
2026-02-10 17:41:14 -0600

    added notes

--- src/main/java/distillation/MeshResidualFunction.java
+++ src/main/java/distillation/MeshResidualFunction.java
@@ -1,5 +1,11 @@
 package distillation;
 
 public class MeshResidualFunction {
-    
+    /*
+    for (int i =0; i < system.getComponentCount(); i++) {
+    double K = thermoProvider.calculateKValue(i, T, P, x, y);
+    //MESH - material balance for component i
+    F[row] = (Liquid flow in * x_in) - (Liquid flow out * x_out) ...
+    }
+    */
 }
--- src/main/java/distillation/ThermodynamicProvider.java
+++ src/main/java/distillation/ThermodynamicProvider.java
@@ -4,4 +4,13 @@ public interface ThermodynamicProvider {
     double calculateKvalue(int componentIndex, double T, double P, double[] x, double[] y);
     double calculateLiquidEnthalpy(double T, double P, double[] x);
     double calculateVaporEnthalpy(double T, double P, double[] y);
+
+
+    /*
+    public double CalculateKvalue() {
+    double[] allLiquidPhis = eos.calculateAllFugacityCoefficients(T,P,x,Phase...)
+    double[] allVaporPhis = eos.calculateAllFugacityCoefficients(T,P,X,phase...)
+    return allLiquidPhis[i] / allVaporPhis[i]
+    }
+    */
 }
