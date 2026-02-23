5b79cf1e58fe96cfe32a039b0613a62567c57322
Luke Siverling <ldsiverling@gmail.com>
2026-02-22 22:18:31 -0600

    unneeded var

--- src/main/java/eos/RKPureParameters.java
+++ src/main/java/eos/RKPureParameters.java
@@ -19,8 +19,6 @@ public final class RKPureParameters implements CubicParameters {
     @Override
     public double a(double T, double[] x) {
         double Tr = T / Tc;
-        double m = 0.480 + 1.574 * omega - 0.176 * omega * omega;
-        //double alpha = Math.pow(1.0 + m * (1.0 - Math.sqrt(Tr)), 2.0);
         double alpha = Math.pow(Tr,-0.5);
         return a0 * alpha;
     }
