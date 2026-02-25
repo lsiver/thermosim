947b7770571662c88e09693a8b242280cd4d2722
Luke Siverling <ldsiverling@gmail.com>
2026-02-25 16:13:59 -0600

    updating distillation

--- src/main/java/distillation/DistillationColumn.java
+++ src/main/java/distillation/DistillationColumn.java
@@ -1,5 +1,42 @@
 package distillation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import database.ChemicalComponent;
+
 public final class DistillationColumn {
+    private final int stageCount;
+    private Stage[] stages;
+
+    public DistillationColumn(int stageCount) {
+        this.stageCount = stageCount;
+
+        for (int i = 0; i < stageCount; i++) {
+            stages[i] = new Stage(300+i*2,101325+i*10,1,1,.5,.5,.5);
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        List<ChemicalComponent> chemicalList = new ArrayList<>();
+        chemicalList.add(new ChemicalComponent("Methane", 16.04, 190.56, 45.99e5, 0.011));
+        chemicalList.add(new ChemicalComponent("Ethane", 30.07, 305.32, 48.72e5, 0.099));
+        
+        ThermodynamicSystem thermoSystem = new ThermodynamicSystem(chemicalList);
+
+        ColumnSpecification columnSpec = new ColumnSpecification(20, new int[]{5}, null, null);
+
+
+
+
+    
+    
+    
+    
+    
+    }
     
 }
--- src/main/java/distillation/RefineryPackage.java
+++ src/main/java/distillation/RefineryPackage.java
@@ -7,17 +7,17 @@ import vlecalcs.RaoultsLawKModel;
 
 public class RefineryPackage implements ThermodynamicProvider{
 
-    VaporPressureCorrelation benzene = AntoineMmHg.of(6.90565, 1211.033, 220.790);
-    VaporPressureCorrelation toluene = AntoineMmHg.of(6.95464, 1344.800, 219.480);
 
-    private final KModel kmodel = new RaoultsLawKModel(new VaporPressureCorrelation[]{benzene, toluene});
 
-    public RefineryPackage() {
+    private final KModel kmodel;
+
+    public RefineryPackage(KModel kmodel) {
+        this.kmodel = kmodel;
     }
 
     @Override
     public double calculateKvalue(int componentIndex, double T, double P, double[] x, double[] y) {
-        return 0.0;
+        return kmodel.K(T, P, x)[componentIndex];
     }
 
     @Override
@@ -30,6 +30,23 @@ public class RefineryPackage implements ThermodynamicProvider{
     public double calculateVaporEnthalpy(double T, double P, double[] y) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'calculateVaporEnthalpy'");
+    }
+
+    public static void main(String[] args) {
+    VaporPressureCorrelation benzene = AntoineMmHg.of(6.90565, 1211.033, 220.790);
+    VaporPressureCorrelation toluene = AntoineMmHg.of(6.95464, 1344.800, 219.480);
+    KModel kmodel = new RaoultsLawKModel(new VaporPressureCorrelation[]{benzene, toluene});
+
+    RefineryPackage testPackage = new RefineryPackage(kmodel);
+
+    System.out.println(testPackage.calculateKvalue(0,300,101325,new double[]{0.0,0.1,0.5,1.0},new double[]{0.0, 0.3,0.7,1.0}));
+    System.out.println(testPackage.calculateKvalue(1,300,101325,new double[]{0.0,0.1,0.5,1.0},new double[]{0.0, 0.3,0.7,1.0}));
+    //double[] x and double[] y does not matter for Raoults law but it is part of the interface contract.
+    // These WILL matter for complicated methods (PR fugacity)
+
+
+
+
     }
 
 }
